47d7e55810cc55db490312fffed185725316f166
So-coder-ai <narianshiv12@gmail.com>
2024-10-06 21:30:29 +0530

    created a much more interactive one

--- RockPapergam.py
+++ RockPapergam.py
@@ -1,22 +1,26 @@
 import random
 print("Welcome to Rock Paper Scissors Game ENJOY !!")
 print("---------------------------------------------")
-print("YOU CAN CHOOSE ANY (rock ,paper,scissors)")
-options=("rock","paper","scissors")
-player=None
-computer=random.choice(options)
-while player not in options:
-    player=input("enter a choice: ")
+print("YOU CAN CHOOSE ANY rock🪨 , paper📃 , scissors✂️ ")
 
+options={1:"rock🪨", 
+         2:"paper📃", 
+         3:"scissors✂️"}
+player = -1
+computer = options.get(random.randint(1,3))
+while not player > 0 or not player <= 3:
+    player = int(input("Enter Your Choice : "))
+
+player = options.get(player)
 print(f"your choice was {player}")
 print(f"computer choice was {computer}")
-if player==computer:
-    print("ITS A TIE!!")
-elif player=="rock" and computer=="scissors":
+if player == computer:
+     print("ITS A TIE!!")
+elif player == "rock🪨" and computer == "scissors✂️":
     print("YOU WIN !!")
-elif player=="scissors"and computer=="paper":
+elif player == "scissors✂️" and computer == "paper📃":
     print("YOU WIN !!")    
-elif player=="paper" and computer == "rock":
-    print("YOU WIN !!")
+elif player == "paper📃" and computer == "rock🪨":
+     print("YOU WIN !!")
 else:
     print("COMPUTER WINS!!")
